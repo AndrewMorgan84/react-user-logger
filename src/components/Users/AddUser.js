2af8dfccabf8d57bbf5282b1d5193b052d26992f
Andrew <andrew.morgan@e.org>
2022-02-01 16:47:28 +0000

    Added wrapper component and implementation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import Button from '../UI/Button';
 import styles from './AddUser.module.css';
 import { useState } from "react";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 const AddUser = (props) => {
 
@@ -45,10 +46,8 @@ const AddUser = (props) => {
         setEnteredAge('');
     };
 
-    
-
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={styles.input}>
                 <form onSubmit={addUserHandler}>
@@ -69,7 +68,7 @@ const AddUser = (props) => {
                 <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>      
+        </Wrapper>      
     );
 };
 
